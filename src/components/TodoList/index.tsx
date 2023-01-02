4511a45799e2244e16c6e364d66bc676e6ec5fc7
qkrwltn8976 <qkrwltn8976@naver.com>
2023-01-02 11:22:39 +0900

    feat: 할일 상세 모달

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
-import { Button, List } from "antd";
-import * as React from "react";
+import { Button, List, Modal } from "antd";
+import React, { useState } from "react";
 import { Todo } from "../../types/todos";
 
 interface ITodoListProps {
@@ -12,18 +12,36 @@ const Base = styled.div`
 `;
 
 const TodoList: React.FunctionComponent<ITodoListProps> = ({ todos }) => {
-  console.log(todos);
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <Base>
       <List
         itemLayout="horizontal"
         dataSource={todos}
         renderItem={(item) => (
-          <List.Item actions={[<Button>수정</Button>, <Button>삭제</Button>]}>
+          <List.Item
+            actions={[
+              <Button onClick={() => setOpen(true)}>보기</Button>,
+              <Button>삭제</Button>,
+            ]}
+          >
             <List.Item.Meta title={item.title} description={item.content} />
           </List.Item>
         )}
       />
+      <Modal
+        title="Modal 1000px width"
+        centered
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={500}
+      >
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
     </Base>
   );
 };
