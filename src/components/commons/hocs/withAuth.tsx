daa9d78a0ae3c6f008c613716d1388533ce7c4af
qkrwltn8976 <qkrwltn8976@naver.com>
2023-01-12 14:07:00 +0900

    refactor: any 타입  제거

--- src/components/commons/hocs/withAuth.tsx
+++ src/components/commons/hocs/withAuth.tsx
@@ -2,21 +2,23 @@ import { useNavigate } from "react-router-dom";
 import React, { ComponentType, useEffect } from "react";
 import { useLocalStorage } from "../../../utils/storage";
 
-
-function WithAuth<P extends object>(
-  Component: ComponentType<P>
-): React.FC<P> {
+function WithAuth<P extends object>(Component: ComponentType<P>): React.FC<P> {
   return function WihAuthComponent({ dataObj, ...props }: any) {
     const navigate = useNavigate();
     const [loginToken, setLoginToken] = useLocalStorage("loginToken", "");
 
     useEffect(() => {
-      if (!loginToken)
-        return navigate("/auth");
+      if (!loginToken) return navigate("/auth");
       else return navigate("/");
-    }, [loginToken])
+    }, [loginToken]);
 
-    return <Component {...props} loginToken={loginToken} setLoginToken={setLoginToken} />;
+    return (
+      <Component
+        {...props}
+        loginToken={loginToken}
+        setLoginToken={setLoginToken}
+      />
+    );
   };
 }
 
