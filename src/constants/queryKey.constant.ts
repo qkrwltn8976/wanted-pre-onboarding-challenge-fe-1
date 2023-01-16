1f8148b23c5e6377cff9659ddce1f22f3158a541
qkrwltn8976 <qkrwltn8976@naver.com>
2023-01-16 17:00:35 +0900

    refactor: todo queryKey constant로 변경

--- src/constants/queryKey.constant.ts
+++ src/constants/queryKey.constant.ts
@@ -2,3 +2,9 @@ export const authKeys = {
   login: ["login"] as const,
   register: ["register"] as const,
 };
+
+export const todoKeys = {
+  all: ["todos"] as const,
+  details: () => [...todoKeys.all, "detail"] as const,
+  detail: (id: string) => [...todoKeys.details(), id] as const,
+};
